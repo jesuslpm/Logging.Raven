6c7b3048c3b34fb692983224a1117266ac149ef5
Jesus López <jesus.lopez@inercya.com>
2024-08-23 14:11:53 +0100

    publish 1.0.3

--- Logging.Raven/publish.cmd
+++ Logging.Raven/publish.cmd
@@ -1,2 +1,2 @@
-dotnet pack -c Release -p:PackageVersion=1.0.1 -p:ContinuousIntegrationBuild=true --output nupkgs
-..\nuget\NuGet.exe Push nupkgs\Logging.RavenDB.1.0.1.nupkg -source https://www.nuget.org
+dotnet pack -c Release -p:PackageVersion=1.0.3 -p:ContinuousIntegrationBuild=true --output nupkgs
+..\nuget\NuGet.exe Push nupkgs\Logging.RavenDB.1.0.3.nupkg -source https://www.nuget.org
--- nuget/Publish.cmd
+++
@@ -1,3 +0,0 @@
-%~dp0.\NuGet.exe Update -self
-%~dp0.\NuGet.exe Push Logging.RavenDB.1.0.2.nupkg -source https://www.nuget.org
-pause
